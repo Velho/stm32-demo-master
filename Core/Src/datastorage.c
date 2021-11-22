806e3b4eb3e8b611b8e8ad44f8aac7e03eac2c64
Velho <jewel960@gmail.com>
2021-11-22 23:04:18 +0200

    Ringbuffer implementation.
    - Renamed the file, without the underscore.
    - Ringbuffer implementation for single structs, not arrays.
    Arrays would require additional work to consider the begin
    and end segments with the size. Tho in this env it would make
    sense to maintain the state with pointers to the tail and head.

---
+++ Core/Src/datastorage.c
@@ -0,0 +1,55 @@
+/*
+ * data_storage.c
+ *
+ *  Created on: Oct 26, 2021
+ *      Author: Velho
+ */
+
+#include "datastorage.h"
+
+#include <assert.h>
+#include <string.h>
+
+#define STORAGE_BUFFER_MASK STORAGE_BUFFER_LENGTH - 1
+
+DataStorageStatus DataStorage_Append(DataStorageList* list, uint8_t* data)
+{
+    DataStorageStatus ret = DATASTORAGE_OK;
+
+    if (list == NULL || data == NULL)
+    {
+        ret = DATASTORAGE_INVALID;
+        return ret;
+    }
+
+    size_t offset = list->write * list->element_size;
+    memcpy(&list->buffer[offset], data, list->element_size);
+    list->write = (list->write + 1) & STORAGE_BUFFER_MASK;
+
+    return ret;
+}
+
+DataStorageStatus DataStorage_Pop(DataStorageList* list, uint8_t* data)
+{
+    DataStorageStatus ret = DATASTORAGE_OK;
+
+    if (list == NULL || data == NULL)
+    {
+        ret = DATASTORAGE_INVALID;
+        return ret;
+    }
+
+    size_t offset = list->read * list->element_size;
+    memcpy(data, &list->buffer[offset], list->element_size);
+    list->read = (list->read + 1) & STORAGE_BUFFER_MASK;
+
+    return ret;
+}
+
+void DataStorage_Init(DataStorageList* list, size_t element_size)
+{
+    list->write = 0;
+    list->read = 0;
+
+    list->element_size = element_size;
+}
